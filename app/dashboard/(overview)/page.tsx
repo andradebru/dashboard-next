f5f81e469e1e021de1cc8ef353a79ee3656cac4e
andradebru <77248375+andradebru@users.noreply.github.com>
2024-04-15 19:02:05 -0300

    apply loading skeleton only to dashboard

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,7 +3,7 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import latestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { LatestInvoiceRaw } from '../lib/definitions';
+import { LatestInvoiceRaw } from '../../lib/definitions';
 import {
   fetchRevenue,
   fetchLatestInvoices,
