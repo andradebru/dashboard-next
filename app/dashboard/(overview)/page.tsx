f3b4961bd6450eb985c5547c6d465335c9f5a0ae
andradebru <77248375+andradebru@users.noreply.github.com>
2024-04-16 19:54:21 -0300

    streams revenueChart and shows loader in it

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,15 +4,11 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import latestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { LatestInvoiceRaw } from '../../lib/definitions';
-import {
-  fetchRevenue,
-  fetchLatestInvoices,
-  fetchCardData,
-} from '@/app/lib/data';
+import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { sql } from '@vercel/postgres';
-
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 export default async function Page() {
-  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
 
   const {
@@ -38,7 +34,9 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>{' '}
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
